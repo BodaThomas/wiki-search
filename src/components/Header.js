861b9667ad882e8f8ebaa341bdeb0430129cd336
Thomas Boda <boda.thomas@orange.fr>
2021-02-18 17:41:54 +0100

    The SearchBar is now working

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,18 @@ import PropTypes from 'prop-types'
 class Header extends React.Component{
     constructor(props) {
         super(props)
+
+        this.state = {
+            redirect: null
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        let query = event.target.query.value
+
+        query = query.replace(/ /g,'_')
+        window.location.href = '/searchResults/' + encodeURIComponent(query)
     }
 
     render() {
@@ -13,10 +25,10 @@ class Header extends React.Component{
                     <h1 className="title">Wiki search</h1>
                     <a className="liked-link" href="/liked">LIKED</a>
                 </header>
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div className="input-container">
                         <label htmlFor="page-title">Page title</label>
-                        <input id="page-title" placeholder="Try &quot;Paris&quot; or &quot;Marseille&quot;" defaultValue={this.props.title}/>
+                        <input type="text" name="query" id="page-title" placeholder="Try &quot;Paris&quot; or &quot;Marseille&quot;" defaultValue={this.props.title}/>
                     </div>
                     <button className="searchButton">SEARCH</button>
                 </form>
